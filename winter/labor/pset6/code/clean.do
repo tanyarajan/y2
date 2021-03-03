62de230612a1bccccf42aba69d8c5eca44e0383f
tanyarajan <tanyarajan@gmail.com>
2021-03-03 11:49:15 -0600

    labor gmm

--- winter/labor/pset6/code/clean.do
+++ winter/labor/pset6/code/clean.do
@@ -1,13 +1,37 @@
 *--------------------------------------------
-*       Cleaning NLSY Income Data
+*       Cleaning Data to Replicate the
+* 				Hryshko Analysis
 *--------------------------------------------
+* Note: ssc install tsspell if you haven't already
+* also may need to install carry forward 
+ssc install tsspell
+ssc install carryforward
+
 clear all 
 global projects : env projects
-global filepath ${projects}/y2/winter/labor/pset6
+global filepath ${projects}/psets_heckmanlabor/pset6
+*--------------------------------------------
+* 				Cleaning CPI data
+*-------------------------------------------- 
+insheet using ${filepath}/data/cpi.csv
+
+* Averaging per year
+collapse (mean) value, by(year)
+rename value cpi
+sort year
+
+* Normalizing 1982 to 100 as in the paper
+local base = cpi
+replace cpi = cpi/`base'
+
+save ${filepath}/data/cpi_adjustment.dta, replace
 
 
+*--------------------------------------------
+* 				Cleaning NLSY data
+*-------------------------------------------- 
 * Loading and naming data
-insheet using ${filepath}/data/nlsy_income.csv
+insheet using ${filepath}/data/nlsy_income.csv, clear
 rename *, upper
 do ${filepath}/code/nlsy_income-value-labels.do
 rename *, lower
@@ -15,36 +39,127 @@ rename *, lower
 * Renaming variables of interest
 rename sample_sex_1979 sex
 rename caseid_1979 caseid
+rename fam_1b_1979 age
 
 * Reshaping
-reshape long q13_5_trunc_revised_ esr_col_, i(caseid sex) j(year)
-keep q13_5_trunc_revised_* esr_col_* caseid sex year
+reshape long q13_5_trunc_revised_ esr_col_ q3_4_ q3_3_, i(caseid sex) j(year)
+keep q13_5_trunc_revised_* esr_col_* q3_4_* q3_3* caseid sex year sampweight age
 
 * Renaming again
 rename q13 income
 rename esr employment
+rename q3_4 edu_complete
+rename q3_3 edu_attend
+rename sampweight weights
+
+***** Age and Education Variables *****
 
 * Restricting sample
 drop if sex == 2 // dropping females
-drop if income == . | income <= 0 // missing income
-drop if employment == 3 // out of labor force
+replace income = . if income <= 0 // missing income
+replace income = . if employment == 3 // out of labor force
+
+* Generating college dummy
+replace edu_complete = . if edu_complete < 0
+replace edu_attend = . if edu_attend < 0
+sort caseid year
+bys caseid: carryforward edu_complete, gen(education) // imputing education for future years
+egen nonmiss_edu = count(edu_complete), by(caseid)
+drop if nonmiss_edu == 0
+
+// College dummy
+gen college = (education > 12)
+replace college = . if education == .
+
+* Generating age polynomials
+replace age = (year - 1979) + age
+drop if age <= 25 | age > 64
+label val age
+gen age2 = age^2
+gen age3 = age^3
+
+***** Keeping Every Other Period *****
+* generating period indicator to deal with 2-year gaps that start in 1996 
+* note the start year is 1982 bc the standardized income variable starts then
+drop if inlist(year, 1979, 1981, 1983, 1985, 1987, 1989, 1991, 1993, 1995)
+
+gen period = year - 1982
+global yrlist 1984 1986 1988 1990 1992 1994 1996 1998 2000 ///
+			2002 2004 2006 2008 2010 2012 2014 2016
+global num : list sizeof global(yrlist)
+forval y = 1/$num{
+	local yr : word `y' of ${yrlist}
+	replace period = `y' if year == `yr'
+}
+xtset caseid period
 
 
-* Keeping if they have at least 9 consecutive obs
-xtset caseid year
+***** Income Variables *****
+
+* Adjusting by CPI
+rename income unadj_inc
+merge m:1 year using ${filepath}/data/cpi_adjustment.dta
+drop if _merge == 2
+drop _merge
+sort caseid period
+gen income = unadj_inc / cpi
+
+* Restricting if real change in income too high or low as in paper
+gen inc_change = (income - L.income)/income * 100
+replace income = . if inc_change > 500 | inc_change < -80 
+
+
+* Log income
+sort caseid period
+gen lninc = ln(income)
+gen d_lninc = lninc - L.lninc
+egen ln_var = sd(lninc), by(year)
+replace ln_var = ln_var^2
+preserve
+collapse (mean) ln_var, by(year)
+*twoway line ln_var year if year >= 1984, graphregion(color(white))
+restore
+
+* Residual income: regression of difference in log earnings on edu/age 
+reg d_lninc age age2 age3 college age#college age2#college age3#college
+predict y, residuals
+
+* Marking consecutive runs of data
+drop if year < 1984
 gen nrun = .
-by caseid: replace nrun = cond(L.nrun == ., 1, L.nrun + 1)
+by caseid: replace nrun = cond(L.nrun == . | y == . | L.y== ., 1, L.nrun + 1)
 by caseid: egen maxrun = max(nrun)
-keep if maxrun > 9 
 
-* Removing obs
-gen runcnt = L.nrun
-drop if runcnt == .
+* Keeping only longest consecutive spell (and only if 9 consecutive observations)
+tsspell income, f(nrun==1)
+egen spellmax = count(_spell), by(caseid _spell)
+keep if spellmax == maxrun
+keep if spellmax > 9 
 
-* Lags
-gen lag1 = L.income
-gen fd_inc = income - L.income
-drop if fd_inc == .
+*--------------------------------------------
+* 		Setting up lags for GMM
+*-------------------------------------------- 
+keep caseid period maxrun y
+sum maxrun
+global maxT = r(max) 
 
-* Save
+* T 
+sort caseid period
+gen T = .
+bys caseid: replace T = _n
+
+drop period maxrun 
+order caseid T y
+
+* Lags needed
+xtset caseid T
+gen ly1 = L.y
+gen ly2 = L2.y
+
+global loopT = $maxT - 1
+forval t=1/$loopT{
+	gen fy`t' = F`t'.y
+}
+
+* Save for analysis in R
 outsheet using ${filepath}/data/nlsy_clean.csv, comma nolabel replace
--- winter/labor/pset6/code/gmm.R
+++ winter/labor/pset6/code/gmm.R
@@ -7,6 +7,8 @@ rm(list=ls())
 require(data.table)
 require(ggplot2)
 require(gsubfn)
+require(knitr)
+require(kableExtra)
 
 # set working directory (change to run)
 filepath <- "/Users/tanyarajan/Documents/git/psets_heckmanlabor/pset6" 
@@ -16,18 +18,24 @@ setwd(filepath)
 df <- as.data.table(read.csv(file = "data/nlsy_clean.csv"))
 
 # Function to extract data
-extract_data <- function(data){
+extract_data <- function(datain){
+  data <- copy(datain)
   y <- data$y
   y_lag1 = data$ly1
   y_lag2 = data$ly2
   y_lead1 = data$fy1
   y_lead2 = data$fy2
+  yforward = as.matrix(data[, c("caseid", "T", "y", "ly1", "ly2", "fy1", "fy2"):= NULL])
+
   # need to figure out yforward for first moment
-  yforward = 0
   return(list(y=y, y_lag1=y_lag1, y_lag2=y_lag2, 
               y_lead1=y_lead1, y_lead2=y_lead2, yforward=yforward))
 }
 
+df2 <- copy(df)
+test = as.matrix(df2[, c("caseid", "T", "y", "ly1", "ly2", "fy1", "fy2"):= NULL])
+
+list[a,b,c,d,e,f] <- extract_data(df)
 
 #####################################################
 #                 Moment Conditions                 #
@@ -43,69 +51,189 @@ parse_omega <- function(omega){
 }
 
 # Moment 1: from covariance gamma_k
-moment1 <- function(omega, y, yforward){
+moment1 <- function(omega, y, yforward, Tmax){
   list[s_b, s_xi, theta, s_ep, s_u] <- parse_omega(omega)
-  # need to fill in
+  
+  gmat <- matrix(NA, ncol=(Tmax-3), nrow=dim(yforward)[1])
+  for (i in 3:Tmax){
+    idx = i - 3
+    gmat[,idx] <- y*yforward[,idx]
+  }
+
+  # currently just using gamma3, but technically shoudl be for gamma_k, k>=3
+  return(gmat - s_b)
 }
 
 # Moment 2: from summation moment (below eqn 9 in the paper)
-moment2 <- function(omega, y, y_lag, y_lag2, y_lead1, y_lead2){
+moment2 <- function(omega, y, y_lag1, y_lag2, y_lead1, y_lead2){
   list[s_b, s_xi, theta, s_ep, s_u] <- parse_omega(omega)
   # need to fill in
   gsum <- y_lag2 + y_lag1 + y + y_lead1 + y_lead2
-  gmom <- mean(y*gsum, na.rm=TRUE)
+  gmom <-y*gsum
   return(gmom - 5*s_b - s_xi)
 }
 
 # Moment 3: from covariance gamma_2
 moment3 <- function(omega, y, y_lag2){
   list[s_b, s_xi, theta, s_ep, s_u] <- parse_omega(omega)
-  g2 <- mean(y*y_lag2, na.rm=TRUE)
+  g2 <- y*y_lag2
   return(g2 - s_b + theta*s_ep)
 }
 
 # Moment 4: from covariance gamma_1
 moment4 <- function(omega, y, y_lag1){
   list[s_b, s_xi, theta, s_ep, s_u] <- parse_omega(omega)
-  g1 <- mean(y*y_lag1, na.rm=TRUE)
+  g1 <- y*y_lag1
   return(g1 - s_b + (theta - 1)^2 * s_ep + s_u)
 }
-
 
 # Moment 5: from covariance gamma_0
 moment5 <- function(omega, y){
   list[s_b, s_xi, theta, s_ep, s_u] <- parse_omega(omega)
-  g0 <- mean(y*y, na.rm=TRUE)
+  g0 <- y*y
   return(g0 - s_xi - s_b - (1 + (1-theta)^2 + theta^2)*s_ep - 2*s_u)
 }
 
 
 #####################################################
-#                 GMM Objective Fn                  #
+#                 Gradient Functions                #
 #####################################################
+# Gradient 1
+grad1 <- function(omega, Tmax){
+  m1num <- Tmax - 3
+  list[s_b, s_xi, theta, s_ep, s_u] <- parse_omega(omega)
+  return(t(matrix(c(1, 0, 0, 0, 0), nrow=5, ncol=m1num))) 
+}
 
-gmm_objective <- function(omega, data){
+# Gradient 2
+grad2 <- function(omega){
+  list[s_b, s_xi, theta, s_ep, s_u] <- parse_omega(omega)
+  return(c(-5, -1, 0, 0, 0))
+}
+
+# Gradient 3
+grad3 <- function(omega){
+  list[s_b, s_xi, theta, s_ep, s_u] <- parse_omega(omega)
+  return(c(-1, 0, s_ep, 0, 0))
+}
+
+# Gradient 4
+grad4 <- function(omega){
+  list[s_b, s_xi, theta, s_ep, s_u] <- parse_omega(omega)
+  return(c(-1, 0, 2*(theta-1)*s_ep, (theta-1)^2, 1))
+}
+
+# Gradient 5
+grad5 <- function(omega){
+  list[s_b, s_xi, theta, s_ep, s_u] <- parse_omega(omega)
+  return(c(-1, -1, 2*(1-theta)*s_ep + 2*theta, 1 + (1-theta)^2 + theta^2, -2))
+}
+
+
+#####################################################
+#              GMM Estimation (EWMD)                #
+#####################################################
+# stacking moments
+gmm_stack <- function(omega, data, Tmax){
   # extract data
-  list[y, y_lag1, y_lag2, y_lead1, y_lead2, yforward] <- extract_data(data)
-  
+  list[y, y_lag1, y_lag2, y_lead1, y_lead2, y_lead3] <- extract_data(data)
+
   # form moments
-  m1 <- moment1(omega, y, yforward)
+  m1 <- moment1(omega, y, y_lead3, Tmax)
   m2 <- moment2(omega, y, y_lag1, y_lag2, y_lead1, y_lead2)
   m3 <- moment3(omega, y, y_lag2)
   m4 <- moment4(omega, y, y_lag1)
   m5 <- moment5(omega, y)
-  mstack = rbind(m1, m2, m3, m4, m5)
-  
-  # objective (weighting matrix = identity)
-  return(dot(mstack, mstack))
+  mstack = cbind(m1, m2, m3, m4, m5)
+  mstack[is.na(mstack)] <- 0
+  return(mstack)
 }
 
+gmm_stack(c(1,2,3,4,5), df, 15)
+
+
+
+#-----------------------------------
+#      Parameter Estimates 
+#-----------------------------------
+# gmm objective
+gmm_objective <- function(omega, data, model, Tmax){
+  
+  # setup
+  if (model == 1){omega[2] <- 0}
+  moments <- colMeans(gmm_stack(omega, data, Tmax), na.rm=T)
+  
+  # GMM estimation
+  return(t(moments)%*%moments)
+}
 
 # optimization
-omega_guess = c(1,2,3,4,5) # obviously change to make this better
-optim(omega_guess, gmm_objective, method="BFGS", lower = 0, hessian = TRUE)
+omega_guess = c(1, 0, 1, 1, 1) # obviously change to make this better
+results1 <-optim(omega_guess, gmm_objective, data=df, Tmax=15, model=1, method="L-BFGS-B", lower = c(0,0,-1,0,0), hessian = TRUE)
+results2 <-optim(omega_guess, gmm_objective, data=df, Tmax=15, model=2, method="L-BFGS-B", lower = c(0,0,-1,0,0), hessian = TRUE)
 
 
-# Optim function arguments (for reference):
-#optim(par, fn, gr = NULL, …,  method = c("Nelder-Mead", "BFGS", "CG", "L-BFGS-B", "SANN",
-#                 "Brent"), lower = 0, upper = Inf, control = list(), hessian = FALSE)
+# displaying coefficients
+coef1 <- results1$par
+coef2 <- results2$par
+
+#-----------------------------------
+#        Standard Errors
+#-----------------------------------
+
+# Sigma function at center of asymptotic variance formula
+compute_meat <- function(omega, data, model, Tmax){
+  # W matrix
+  mstack <- gmm_stack(omega, data, Tmax)
+  sqsum <- matrix(0, dim(mstack)[2], dim(mstack)[2])
+  for (i in 1:dim(data)[1]){
+    sqsum <- sqsum + mstack[i,]%*%t(mstack[i,])
+  }
+  meat <- (1/dim(data)[1])*sqsum
+  return(meat)
+}
+
+# computing standard errors
+gmm_se <- function(omega, data, model, Tmax){
+  # meat
+  meat<-compute_meat(omega, data, model, Tmax)
+  
+  # gradient
+  G <- rbind(grad1(omega, Tmax), grad2(omega), grad3(omega), grad4(omega), grad5(omega))
+  if (model == 1){
+    G <- rbind(grad1(omega, Tmax), grad2(omega), grad3(omega), grad4(omega), grad5(omega))
+    G <- G[,-2]
+    }
+
+  # variance
+  bread <- G%*%solve(t(G)%*%G, tol=1e-20)
+  variance <- t(bread)%*%meat%*%bread
+  return(sqrt(diag(variance)/dim(data)[1]))
+
+}
+
+se1 <- gmm_se(results1$par, df, 1, 15)
+se2 <- gmm_se(results2$par, df, 2, 15)
+
+             
+fmtse1 <- c(0,0,0,0)
+for (j in 1:length(se1)){
+  fmtse1[j] <- as.character(paste0("(",round(se1[j], 4),")"))
+}
+fmtse1 <- append(fmtse1, "-", after=1)
+
+fmtse2 <- c(0,0,0,0,0)
+for (j in 1:length(se2)){
+  fmtse2[j] <- as.character(paste0("(",round(se2[j], 4),")"))
+}
+
+outtable <- rbind(round(coef1, 4), fmtse1, round(coef2,4), fmtse2)
+rownames(outtable) <- c("HIP Model", "  ", "HIP + RW Model", " ")
+#outtable <- cbind(names, outtable)
+colnames(outtable)<-c( "$\\sigma_\\beta$","$\\sigma_\\xi$","$\\theta$","$\\sigma_\\varepsilon$", "$\\sigma_u$")
+
+kable(outtable, format="latex", booktabs=TRUE, escape=FALSE) %>%
+  write(file="tables/tr_table2.tex")
+
+
+
